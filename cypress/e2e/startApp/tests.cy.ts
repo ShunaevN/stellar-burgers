792d511d2f91d94a0f57dbcd216b8387952d50e6
nikita shunaev <sowatid942@ociun.com>
2025-02-07 13:50:00 +0500

    second iteration

--- cypress/e2e/startApp/tests.cy.ts
+++ cypress/e2e/startApp/tests.cy.ts
@@ -2,18 +2,21 @@ beforeEach(()=>{
     cy.intercept('GET', 'api/ingredients', { fixture: 'ingredients.json' });
     cy.visit('http://localhost:4000');
     cy.viewport(2000, 1000);
+
+    cy.get('[data-cy="burger_ingredient_Краторная булка N-200i"]').as('bun');
+    cy.get('[data-cy="burger_ingredient_Биокотлета из марсианской Магнолии"]').as('main');
+    cy.get('[data-cy="burger_ingredient_Соус Spicy-X"]').as('sauce');
+    cy.get('[data-cy="constructor_ingredients_price"]').as('ingredients_price');
+    
 });
 
 
 describe('проверяем работу конструктора', function() {
     it('Добавление данных в конструктор', function() {
-       const bun = cy.get('[data-cy="burger_ingredient_Краторная булка N-200i"]');
-       const main = cy.get('[data-cy="burger_ingredient_Биокотлета из марсианской Магнолии"]');
-       const sauce = cy.get('[data-cy="burger_ingredient_Соус Spicy-X"]');
-       bun.contains('Добавить').click();
-       main.contains('Добавить').click();
-       sauce.contains('Добавить').click();
-       cy.get('[data-cy="constructor_ingredients_price"]').contains('3024').should('exist');
+       cy.get('@bun').contains('Добавить').click();
+       cy.get('@main').contains('Добавить').click();
+       cy.get('@sauce').contains('Добавить').click();
+       cy.get('@ingredients_price').contains('3024').should('exist');
     });
 }); 
 
@@ -22,22 +25,20 @@ describe('проверка создания заказа', function() {
      it('Создание заказа', function() {
         cy.intercept('GET', 'api/auth/user', { fixture: 'user.json' });
 
-        const bun = cy.get('[data-cy="burger_ingredient_Краторная булка N-200i"]');
-        const main = cy.get('[data-cy="burger_ingredient_Биокотлета из марсианской Магнолии"]');
-        const sauce = cy.get('[data-cy="burger_ingredient_Соус Spicy-X"]');
-
-        bun.contains('Добавить').click();
-        main.contains('Добавить').click();
-        sauce.contains('Добавить').click();
-
+        cy.get('@bun').contains('Добавить').click();
+        cy.get('@main').contains('Добавить').click();
+        cy.get('@sauce').contains('Добавить').click();
         cy.get('[data-cy="total_order_price"]').click();
+
         cy.intercept('POST', 'api/orders', { fixture: 'order.json' });
-        const isModal = cy.get('[data-cy="ingredient_detail"]');
-        isModal.should('exist');
+        cy.get('[data-cy="ingredient_detail"]').as('modal');
+        cy.get('[data-cy="ingredient_detail_button_close"]').as('modal_close');
+        cy.get('@modal').should('exist');
         cy.get('[data-cy="order_number"]').contains('5398');
-        cy.get('[data-cy="ingredient_detail_button_close"]').click();
-        isModal.should('not.exist');
-        cy.get('[data-cy="constructor_ingredients_price"]').contains('0');
+
+        cy.get('@modal_close').click();
+        cy.get('@modal').should('not.exist');
+        cy.get('@ingredients_price').contains('0');
      });
 
 }); 
@@ -45,14 +46,15 @@ describe('проверка создания заказа', function() {
 
 describe('проверка модального окна', function() {
     it('Открытие и закрытие по крестику', function() {
-        const bun = cy.get('[data-cy="burger_ingredient_Краторная булка N-200i"]');
-        bun.click();
-        cy.get('[data-cy="ingredient_detail"]').get('[data-cy="ingredient_detail_button_close"]').click();
+
+        cy.get('@bun').click();
+        cy.get('[data-cy="ingredient_detail"]').as('modal');
+        cy.get('[data-cy="ingredient_detail_button_close"]').as('modal_close');
+        cy.get('@modal').get('@modal_close').click();
      });
 
     it('Открытие и закрытие по оверлею', function() {
-        const bun = cy.get('[data-cy="burger_ingredient_Краторная булка N-200i"]');
-        bun.click();
+        cy.get('@bun').click();
         cy.get('[data-cy="overlay_close"]').click({force:true});
      });
 });
